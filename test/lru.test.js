bbee368d8ced350ac592ff4805e7be144b05fbf5
Vladislav Kamenev <mazahakaforever@ya.ru>
2017-09-16 06:15:19 +0300

    Fixed LRU implementation. Fixed size calculation on expired elements.

--- test/lru.test.js
+++ test/lru.test.js
@@ -22,6 +22,16 @@ describe('Least recently used expiration', function () {
     should.equal(cache.get('a'), undefined)
   })
 
+  it('should update element status when get', function () {
+    cache.set('a', 1)
+    cache.set('b', 2)
+    cache.set('c', 3)
+    cache.get('a')
+    cache.set('d', 4)
+    should.notEqual(cache.get('a'), undefined)
+    should.equal(cache.get('b'), undefined)
+  })
+
   afterEach(function () {
     cache.clear()
   })
